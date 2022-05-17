8d71d2b331f79cc3514bc3016acdf4354f5cb7d3
Le Mien <lemien@Les-MacBook-Air.local>
2022-05-17 17:37:59 +0700

    u

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,7 +37,7 @@
                 window.clearTimeout(timer);
             }
             timer = window.setTimeout(function() {
-                if(scrollTop >= 330) {
+                if(scrollTop >= 260) {
                     head.addClass('head__fix');
                 } else {
                     head.removeClass('head__fix');
@@ -54,39 +54,6 @@
         });
     };
 
-    var owlCarousel = function() {
-        // SINGLE PAGE - Gallery //
-        $('.single-post__gallery--js').owlCarousel({
-            loop: true,
-            margin: 0,
-            dots: true,
-            nav: true,
-            lazyLoad: true,
-            autoplay: true,
-            items: 1,
-            navText : ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
-        });
-    };
-
-    var masonry = function () {
-        $('.grid-js').masonry({
-            itemSelector: '.post-type-two__item',
-            columnWidth: '.grid-size',
-        });
-    };
-
-    var searchDesktop = function () {
-        $('.head__button-search').click(function(){
-            $('body').toggleClass('search-js-open');
-        });
-    };
-
-    var sidebarScroll = function() {
-        $('.sidebar-fixed').theiaStickySidebar({
-            additionalMarginTop: 20
-        });
-    }
-
     var skill = function () {
         var skill_item = $('.my-resume__skill--item');
         skill_item.each(function (k, v) {
@@ -99,28 +66,6 @@
                 }
             });
         });
-    }
-
-    var sticky = function () {
-        apply_stickies()
-        window.addEventListener('scroll', function() {
-            apply_stickies()
-        })
-        function apply_stickies() {
-            var _$stickies = [].slice.call(document.querySelectorAll('.sticky-top'))
-            _$stickies.forEach(function(_$sticky) {
-                if (CSS.supports && CSS.supports('position', 'sticky')) {
-                    apply_sticky_class(_$sticky)
-                }
-            })
-        }
-        function apply_sticky_class(_$sticky) {
-            var currentOffset = _$sticky.getBoundingClientRect().top
-            var stickyOffset = parseInt(getComputedStyle(_$sticky).top.replace('px', ''))
-            var isStuck = currentOffset <= stickyOffset
-
-            _$sticky.classList.toggle('is-sticky', isStuck)
-        }
     }
 
     var animation = function () {
@@ -152,12 +97,7 @@
         loading();
         nav();
         lazy();
-        // owlCarousel();
-        // masonry();
-        // searchDesktop();
-        // sidebarScroll();
         skill();
-        // sticky();
         scrollTo();
         $(document).on( 'scroll', function(){
             animation();
